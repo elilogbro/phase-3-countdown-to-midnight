1f9784dd5b7d8d5d4c045c3bf7cf9e993e30678b
Eli <+>
2022-08-29 09:19:25 -0400

    commit tree 0c5550c690b1ff13719e75503702ce8eb60474eb

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+    while integer > 0
+        puts "#{integer} SECOND(S)!"
+        integer -= 1
+    end
+
+    "HAPPY NEW YEAR!"
+
 end
